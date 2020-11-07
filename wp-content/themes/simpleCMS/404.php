fb8e396f4838ca8f16829df662273766c7cc33ae
Morozov312 <alexander.morozov312@gmail.com>
2020-11-07 22:09:48 +0700

    page 404 done

---
+++ wp-content/themes/simpleCMS/404.php
@@ -0,0 +1,7 @@
+<?php get_header('404') ?>
+<div class="section-404">
+    <h1>404</h1>
+    <h2>Oops! Page not found!</h2>
+    <a href="<?php echo get_home_url() ?>">Back to homepage</a>
+</div>
+<?php get_footer('404') ?>
---
+++ wp-content/themes/simpleCMS/header-404.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html id="html" lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>404 page not found!</title>
+    <meta name="description" content=" ">
+    <meta name="keywords" content=" ">
+    <?php wp_head(); ?>
+</head>
+
+<body class="page-404">
