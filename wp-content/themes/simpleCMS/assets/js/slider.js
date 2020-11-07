45cb3e07d02d9ea26dca933287bb2744ba4e6352
Morozov312 <alexander.morozov312@gmail.com>
2020-11-08 02:23:54 +0700

    update slider adaptive upload

--- wp-content/themes/simpleCMS/assets/js/slider.js
+++ wp-content/themes/simpleCMS/assets/js/slider.js
@@ -1,14 +1,34 @@
-$(document).ready(function () {
-    if (window.matchMedia('(max-width: 768px)').matches) {
-        $('.tiles__wrap').addClass('slider');
-    }
-    if (window.matchMedia('(min-width: 768px)').matches) {
-        $('.tiles__wrap').removeClass('slider');
-    }
+function addSlider() {
     $('.slider').slick({
         autoplay: true,
         autoplaySpeed: 4000,
         dots: true,
         arrows: false
-    });
-});
+    })
+}
+$(document).ready(function () {
+    let initialWidth = $(window).currWidth()
+    let needReload = false
+    let isExist = false
+    if (initialWidth < 768) {
+        $('.tiles__wrap').addClass('slider')
+        addSlider()
+        isExist = true
+        needReload = true
+    }
+    $(window).resize(function () {
+        let currWidth = $(window).currWidth()
+        if (currWidth < 768) {
+            $('.tiles__wrap').addClass('slider')
+            if ($('.tiles__wrap') && !isExist) {
+                addSlider()
+                isExist = true
+                needReload = true
+            }
+        } else if (currWidth > 768 && needReload) {
+            $('.tiles__wrap').removeClass('slider');
+            location.reload()
+            needReload = false
+        }
+    })
+})
