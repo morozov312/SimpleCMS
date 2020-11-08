01b1e9aace8627abbf98be8307ea4b4258abf33d
Morozov312 <alexander.morozov312@gmail.com>
2020-11-09 02:46:02 +0700

    try to adaptive about page

--- wp-content/themes/simpleCMS/page-about.php
+++ wp-content/themes/simpleCMS/page-about.php
@@ -1,10 +1,32 @@
 <?php get_header('short') ?>
+<h1 class="about-title">about our store</h1>
 <div class="about-content">
-    <picture>
-        <source srcset="<?php echo get_template_directory_uri() . '/assets/images/about/about.webp' ?>" type="image/webp">
-        <source srcset="<?php echo get_template_directory_uri() . '/assets/images/about/about.jpg' ?>" type="image/jpg">
-        <img src="<?php echo get_template_directory_uri() . '/assets/images/about/about.jpg' ?>" alt="about">
-    </picture>
-    
+    <img src="<?php echo get_template_directory_uri() . '/assets/images/about/about.jpg' ?>" alt="about">
+    <div class="about__description">
+        <h2>Welcome to ICNDY</h2>
+        <h3>Already 6 years we trying <br>
+            to make your shopping <br>
+            comfortable and easer!</h3>
+    </div>
+</div>
+<div class="statistics-list">
+    <ul>
+        <li>
+            <h4>20 thousand+</h4>
+            <span>Brands in store</span>
+        </li>
+        <li>
+            <h4>100 thousand+</h4>
+            <span>Products to choose</span>
+        </li>
+        <li>
+            <h4>930 thousand+</h4>
+            <span>Visitors per day</span>
+        </li>
+        <li>
+            <h4>6 year+</h4>
+            <span>Flawless work</span>
+        </li>
+    </ul>
 </div>
 <?php get_footer() ?>
