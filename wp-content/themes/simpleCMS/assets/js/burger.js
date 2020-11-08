ac769e50f2d181628d2cfe9187ed5a8d5368833f
Morozov312 <alexander.morozov312@gmail.com>
2020-11-09 03:01:48 +0700

    begin adaptive short header

--- wp-content/themes/simpleCMS/assets/js/burger.js
+++ wp-content/themes/simpleCMS/assets/js/burger.js
@@ -1,8 +1,10 @@
 let burger = document.querySelector('.burger')
 let title = document.querySelector('.header__title')
 let nav = document.querySelector('.header__nav')
+let shortHeader = document.querySelector('.short-header')
 burger.addEventListener('click', () => {
     burger.classList.toggle('active-burger')
-    title.classList.toggle('hidden-title')
+    if (title) title.classList.toggle('hidden-title')
     nav.classList.toggle('mobile-nav')
+    if (shortHeader) shortHeader.classList.toggle('tall-header')
 })
