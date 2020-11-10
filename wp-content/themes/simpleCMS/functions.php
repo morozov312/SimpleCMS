222e6a5206931e89c0cb25d9ab8dd011314fbff8
Morozov312 <alexander.morozov312@gmail.com>
2020-11-11 01:11:58 +0700

    split css files

--- wp-content/themes/simpleCMS/functions.php
+++ wp-content/themes/simpleCMS/functions.php
@@ -9,8 +9,12 @@ add_action('after_setup_theme', function () {
 });
 function get_styles()
 {
-    wp_enqueue_style('style', get_stylesheet_uri());
-    wp_enqueue_style('main', get_template_directory_uri() . '/assets/styles/style.css');
+    if (is_404()) {
+        wp_enqueue_style('404', get_template_directory_uri() . '/assets/styles/404.css');
+    } else {
+        wp_enqueue_style('style', get_stylesheet_uri());
+        wp_enqueue_style('main', get_template_directory_uri() . '/assets/styles/style.css');
+    }
 }
 function get_scripts()
 {
@@ -21,5 +25,7 @@ function get_scripts()
         wp_enqueue_script('slick', get_template_directory_uri() . '/assets/js/slick.min.js', '[jquery]', null, true);
         wp_enqueue_script('slider', get_template_directory_uri() . '/assets/js/slider.js', '[jquery]', null, true);
     }
-    wp_enqueue_script('burger', get_template_directory_uri() . '/assets/js/burger.js', null, null, false);
+    if (!is_404()) {
+        wp_enqueue_script('burger', get_template_directory_uri() . '/assets/js/burger.js', null, null, false);
+    }
 }
