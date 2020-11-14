b983ac2beee45509d07d17d62bf77711b1b2de30
Morozov312 <alexander.morozov312@gmail.com>
2020-11-14 15:23:51 +0700

    fixed jquery problems

--- wp-content/themes/simpleCMS/assets/js/slider.js
+++ wp-content/themes/simpleCMS/assets/js/slider.js
@@ -1,37 +1,37 @@
 function addSlider() {
-    $('.slider').slick({
+    jQuery('.slider').slick({
         autoplay: true,
         autoplaySpeed: 4000,
         dots: true,
         arrows: false
     })
 }
-$(document).ready(function () {
-    let initialWidth = $(window).width()
+jQuery(document).ready(function () {
+    let initialWidth = jQuery(window).width()
     let needReload = false
     let isExist = false
-    let initialTiles = $('.tiles__wrap').html()
+    let initialTiles = jQuery('.tiles__wrap').html()
     if (initialWidth < 768) {
-        $('.tiles__wrap').addClass('slider')
+        jQuery('.tiles__wrap').addClass('slider')
         addSlider()
         isExist = true
         needReload = true
     }
-    $(window).resize(function () {
-        let width = $(window).width()
+    jQuery(window).resize(function () {
+        let width = jQuery(window).width()
         if (width < 768) {
-            $('.tiles__wrap').addClass('slider')
+            jQuery('.tiles__wrap').addClass('slider')
             if (!isExist) {
                 addSlider()
                 isExist = true
                 needReload = true
             }
         } else if (width > 768 && needReload) {
-            let removedWrap = $('.tiles__wrap')
+            let removedWrap = jQuery('.tiles__wrap')
             removedWrap.removeClass()
             removedWrap.addClass('tiles__wrap content')
-            $('.tiles__wrap').empty()
-            $('.tiles__wrap').prepend(initialTiles)
+            jQuery('.tiles__wrap').empty()
+            jQuery('.tiles__wrap').prepend(initialTiles)
             needReload = false
             isExist = false
         }
